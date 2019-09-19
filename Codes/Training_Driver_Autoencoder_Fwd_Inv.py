3f4c04c7a2c850c713ab3f4836cd2ab7ef827c48
HwanGoh <Hwan.Goh@gmail.com>
2019-09-19 13:45:37 -0500

    small update

--- Codes/Training_Driver_Autoencoder_Fwd_Inv.py
+++ Codes/Training_Driver_Autoencoder_Fwd_Inv.py
@@ -40,8 +40,8 @@ class RunOptions:
     truncation_layer = 2 # Indexing includes input and output layer
     num_hidden_nodes = 200
     penalty = 10
-    num_training_data = 5000
-    batch_size = 5000
+    num_training_data = 20
+    batch_size = 20
     num_epochs = 50000
     gpu    = '3'
     
