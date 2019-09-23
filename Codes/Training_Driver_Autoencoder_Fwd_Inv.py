2276f558811a2d36800be7d4ccb0e82caca1b7ce
Hwan Goh <Hwan.Goh@gmail.com>
2019-09-22 19:27:54 -0500

    small update

--- Codes/Training_Driver_Autoencoder_Fwd_Inv.py
+++ Codes/Training_Driver_Autoencoder_Fwd_Inv.py
@@ -153,7 +153,7 @@ def trainer(hyper_p, filenames):
         
         # Optimize with LBFGS
         print('Optimizing with LBFGS\n')   
-        #optimizer_LBFGS.minimize(sess, feed_dict=tf_dict)
+        optimizer_LBFGS.minimize(sess, feed_dict=tf_dict)
         [loss_value, s] = sess.run([loss,summ], tf_dict)
         writer.add_summary(s,hyper_p.num_epochs)
         print('LBFGS Optimization Complete\n') 
