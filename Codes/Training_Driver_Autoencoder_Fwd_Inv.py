3d358aaaa6a1a6d3cfc4b8110be9105230e37e34
Hwan Goh <Hwan.Goh@gmail.com>
2019-09-29 12:38:01 -0500

    small update

--- Codes/Training_Driver_Autoencoder_Fwd_Inv.py
+++ Codes/Training_Driver_Autoencoder_Fwd_Inv.py
@@ -215,7 +215,7 @@ def trainer(hyper_p, filenames, use_bnd_data, full_domain_dimensions, state_data
 if __name__ == "__main__":     
     
     use_bnd_data = 0
-    num_testing_data = 20
+    num_testing_data = 200
     
     full_domain_dimensions = 1446    
     if use_bnd_data == 1:
