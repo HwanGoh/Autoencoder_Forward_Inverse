a854ef9338f3b87407ad48afcefa22c26668613c
HwanGoh <Hwan.Goh@gmail.com>
2019-10-04 15:28:09 -0500

    small update

--- Codes/gradient_checker.py
+++ Codes/gradient_checker.py
@@ -6,6 +6,10 @@ Created on Fri Oct  4 12:25:44 2019
 @author: hwan
 """
 
+import numpy as np
+import pdb #Equivalent of keyboard in MATLAB, just add "pdb.set_trace()"
+
+
 ###############################################################################
 #                         Check Gradients Function                            #
 ###############################################################################
@@ -14,43 +18,34 @@ def check_gradients_objects(layers):
     rand_v_weights = []
     rand_v_biases = []      
     for l in range(0, len(layers) - 1): 
-        rand_v_weights = np.random.random_sample(layers[l], layers[l + 1])
-        rand_v_biases = np.random.random_sample(1, layers[l + 1])                                 
+        W = np.random.random_sample((layers[l], layers[l + 1]))
+        b = np.random.random_sample((1, layers[l + 1]))                                 
         rand_v_weights.append(W)
         rand_v_biases.append(b)
     return perturb_h, rand_v_weights, rand_v_biases
 
-def check_gradients(sess, gradients_tf, loss_value, layers, perturb_weights_operation_tf, assign_weights_back_operation_tf, tf_dict):
+def check_gradients(sess, NN, loss, gradients_tf, loss_value, tf_dict):
     #===  Finite Difference Directional Derivative Approximation of Gradient ===#
-    sess.run(perturb_weights_operation_tf, feed_dict=tf_dict)
-    perturbed_loss = sess.run(loss, feed_dict=tf_dict)
-    finite_difference_grad = (perturbed_loss - loss_value)/h
-    sess.run(assign_weights_back_operation_tf, feed_dict=tf_dict)
-    
-    #===  Directional Derivative with Tensorflow Gradient ===#
-    stacked_w_and_b_dimensions = 0
-    for l in range(0, len(layers) - 1):
-        stacked_w_and_b_dimensions = stacked_w_and_b_dimensions + layers[l]*layers[l+1] + layers[l+1]
-    
-    grad_all_w_and_b = np.array([])    
-    for l in range(0, len(layers) - 1):
-        W1 = sess.run('autoencoder/encoder/W1:0', feed_dict=tf_dict)
-        grad_all_w_and_b = np.concatenate([grad_all_w_and_b, vectorized_weights])
-
-    for l in range(1, len(layers)):
-   
-
-###############################################################################
-#                 Assigning Weights for Evaluation of Loss                    #
-###############################################################################    
-def perturb_weights(NN, weights_current, biases_current, perturb_h, rand_v_weights, rand_v_biases):
+    perturb_h, rand_v_weights, rand_v_biases = check_gradients_objects(NN.layers)
+    weights_current = NN.weights
+    biases_current = NN.biases
     for l in range(0, len(NN.weights)): 
-       NN.weights[l].assign(weights_current[l] + rand_v_weights[l]) 
-       NN.biases[l].assign(biases_current[l] + rand_v_biases[l])
-    return 1 # the operations above do absolutely nothing, in the end, all this function does is assign the number 1 to update_weights_flag. Jonathan Wittmer is a very clever boy
-   
-def assign_weights_back(NN, weights_current, biases_current):
+       pdb.set_trace() 
+       NN.weights[l].assign(weights_current[l] + 100*rand_v_weights[l]) 
+       NN.biases[l].assign(biases_current[l] + 100*rand_v_biases[l])
+    perturbed_loss = sess.run(loss, feed_dict=tf_dict)
+    finite_difference_grad = (perturbed_loss - loss_value)/perturb_h
     for l in range(0, len(NN.weights)): 
        NN.weights[l].assign(weights_current[l]) 
        NN.biases[l].assign(biases_current[l])
-    return 1 # the operations above do absolutely nothing, in the end, all this function does is assign the number 1 to update_weights_flag. Jonathan Wittmer is a very clever boy
+    
+    #===  Directional Derivative with Tensorflow Gradient ===#  
+    grad_tf_norm = 0    
+    for l in range(0, len(NN.layers) - 1, 2):
+        W = sess.run(NN.weights[l])
+        b = sess.run(NN.weights[l+1])
+        W_grad_tf = sess.run(gradients_tf[l][0])
+        b_grad_tf = sess.run(gradients_tf[l+1][0])
+        grad_tf_norm = grad_tf_norm + np.sum(np.multiply(W, W_grad_tf)) + np.sum(np.multiply(b, b_grad_tf))
+        
+    print(abs(finite_difference_grad - grad_tf_norm)/(abs(finite_difference_grad)))
