baed0bf8c71afaf6b853819c407548b66048afba
Hwan Goh <Hwan.Goh@gmail.com>
2019-11-03 16:14:34 -0600

    small update

--- Codes_TF2/Training_Driver_Autoencoder_Fwd_Inv.py
+++ Codes_TF2/Training_Driver_Autoencoder_Fwd_Inv.py
@@ -24,10 +24,10 @@ np.random.seed(1234)
 #                       Hyperparameters and Run_Options                       #
 ###############################################################################
 class HyperParameters:
-    data_type         = 'bndonly'
+    data_type         = 'full'
     num_hidden_layers = 3
     truncation_layer  = 2 # Indexing includes input and output layer with input layer indexed by 0
-    num_hidden_nodes  = 200
+    num_hidden_nodes  = 1446
     penalty           = 1
     num_training_data = 50000
     batch_size        = 1000
