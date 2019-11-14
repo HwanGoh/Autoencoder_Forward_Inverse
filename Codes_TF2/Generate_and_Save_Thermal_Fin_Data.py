b13d9dff3080eed96cbda57382d55ddccccc7b09
HwanGoh <Hwan.Goh@gmail.com>
2019-11-14 17:21:52 -0600

    small update

--- Codes_TF2/Generate_and_Save_Thermal_Fin_Data.py
+++ Codes_TF2/Generate_and_Save_Thermal_Fin_Data.py
@@ -101,12 +101,12 @@ if __name__ == "__main__":
     num_data = 50000
         
     # Select true or test set
-    generate_train_data = 1
-    generate_test_data = 0
+    generate_train_data = 0
+    generate_test_data = 1
 
     # Select observation type
-    generate_full_domain = 0
-    generate_boundary_state = 1
+    generate_full_domain = 1
+    generate_boundary_state = 0
     
     # Select parameter type
     generate_nine_parameters = 0
