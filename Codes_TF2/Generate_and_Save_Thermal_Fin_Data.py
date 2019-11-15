dc134927158bfc54bf2a8e4f1d2c181ea9f74174
HwanGoh <Hwan.Goh@gmail.com>
2019-11-15 15:34:03 -0600

    small update

--- Codes_TF2/Generate_and_Save_Thermal_Fin_Data.py
+++ Codes_TF2/Generate_and_Save_Thermal_Fin_Data.py
@@ -99,19 +99,19 @@ def convert_array_to_dolfin_function(V, nodal_values):
 if __name__ == "__main__":  
 
     #=== Number of Data ===#
-    num_data = 50000
+    num_data = 200
     
     #===  Select parameter type ===#
     generate_nine_parameters = 0
     generate_varying = 1
         
     #=== Select true or test set ===#
-    generate_train_data = 1
-    generate_test_data = 0
+    generate_train_data = 0
+    generate_test_data = 1
 
     #===  Select observation type ===#
-    generate_full_domain = 0
-    generate_boundary_state = 1
+    generate_full_domain = 1
+    generate_boundary_state = 0
     
     #===  Defining filenames and creating directories ===#
     if not os.path.exists('../Data'):
