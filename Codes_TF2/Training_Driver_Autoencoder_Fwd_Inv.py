c1cd11f58b2a29d5df7e5ef79c432b76ce4ae85d
Hwan Goh <Hwan.Goh@gmail.com>
2019-11-14 22:21:56 -0600

    going to try vary runs

--- Codes_TF2/Training_Driver_Autoencoder_Fwd_Inv.py
+++ Codes_TF2/Training_Driver_Autoencoder_Fwd_Inv.py
@@ -24,21 +24,18 @@ np.random.seed(1234)
 #                       Hyperparameters and Run_Options                       #
 ###############################################################################
 class HyperParameters:
-    data_type         = 'bndonly'
-    num_hidden_layers = 3
-    truncation_layer  = 2 # Indexing includes input and output layer with input layer indexed by 0
+    data_type         = 'full'
+    num_hidden_layers = 5
+    truncation_layer  = 3 # Indexing includes input and output layer with input layer indexed by 0
     num_hidden_nodes  = 500
     penalty           = 1
     num_training_data = 50000
     batch_size        = 1000
-    num_epochs        = 4000
-    gpu               = '1'
+    num_epochs        = 500
+    gpu               = '0'
     
 class RunOptions:
     def __init__(self, hyper_p): 
-        #===  Number of Testing Data ===#
-        self.num_testing_data = 200
-        
         #=== Use LBFGS Optimizer ===#
         self.use_LBFGS = 0
         
@@ -46,9 +43,10 @@ class RunOptions:
         self.random_seed = 1234
         
         #=== Data type ===#
-        self.use_full_domain_data = 0
-        self.use_bnd_data = 0
-        self.use_bnd_data_only = 0
+        data_thermal_fin_nine = 0
+        data_thermal_fin_vary = 1
+        self.num_testing_data = 200
+        
         if hyper_p.data_type == 'full':
             self.use_full_domain_data = 1
         if hyper_p.data_type == 'bnd':
@@ -64,6 +62,12 @@ class RunOptions:
             self.state_obs_dimensions = 614
         
         #=== File name ===#
+        if data_thermal_fin_nine == 1:
+            self.dataset = 'thermalfin9'
+            parameter_type = ''
+        if data_thermal_fin_vary == 1:
+            self.dataset = 'thermalfinvary'
+            parameter_type = '_vary'
         if hyper_p.penalty >= 1:
             hyper_p.penalty = int(hyper_p.penalty)
             penalty_string = str(hyper_p.penalty)
@@ -71,21 +75,21 @@ class RunOptions:
             penalty_string = str(hyper_p.penalty)
             penalty_string = 'pt' + penalty_string[2:]
 
-        self.filename = hyper_p.data_type + '_hl%d_tl%d_hn%d_p%s_d%d_b%d_e%d' %(hyper_p.num_hidden_layers, hyper_p.truncation_layer, hyper_p.num_hidden_nodes, penalty_string, hyper_p.num_training_data, hyper_p.batch_size, hyper_p.num_epochs)
+        self.filename = self.dataset + '_' + hyper_p.data_type + '_hl%d_tl%d_hn%d_p%s_d%d_b%d_e%d' %(hyper_p.num_hidden_layers, hyper_p.truncation_layer, hyper_p.num_hidden_nodes, penalty_string, hyper_p.num_training_data, hyper_p.batch_size, hyper_p.num_epochs)
 
         #=== Loading and saving data ===#
         if self.use_full_domain_data == 1:
             self.observation_indices_savefilepath = '../../Datasets/Thermal_Fin/' + 'thermal_fin_full_domain'
-            self.parameter_train_savefilepath = '../../Datasets/Thermal_Fin/' + 'parameter_train_%d' %(hyper_p.num_training_data) 
-            self.state_obs_train_savefilepath = '../../Datasets/Thermal_Fin/' + 'state_train_%d' %(hyper_p.num_training_data) 
-            self.parameter_test_savefilepath = '../../Datasets/Thermal_Fin/' + 'parameter_test_%d' %(self.num_testing_data) 
-            self.state_obs_test_savefilepath = '../../Datasets/Thermal_Fin/' + 'state_test_%d' %(self.num_testing_data) 
+            self.parameter_train_savefilepath = '../../Datasets/Thermal_Fin/' + 'parameter_train_%d' %(hyper_p.num_training_data) + parameter_type
+            self.state_obs_train_savefilepath = '../../Datasets/Thermal_Fin/' + 'state_train_%d' %(hyper_p.num_training_data) + parameter_type
+            self.parameter_test_savefilepath = '../../Datasets/Thermal_Fin/' + 'parameter_test_%d' %(self.num_testing_data) + parameter_type
+            self.state_obs_test_savefilepath = '../../Datasets/Thermal_Fin/' + 'state_test_%d' %(self.num_testing_data) + parameter_type
         if self.use_bnd_data == 1 or self.use_bnd_data_only == 1:
             self.observation_indices_savefilepath = '../../Datasets/Thermal_Fin/' + 'thermal_fin_bnd_indices'
-            self.parameter_train_savefilepath = '../../Datasets/Thermal_Fin/' + 'parameter_train_bnd_%d' %(hyper_p.num_training_data) 
-            self.state_obs_train_savefilepath = '../../Datasets/Thermal_Fin/' + 'state_train_bnd_%d' %(hyper_p.num_training_data) 
-            self.parameter_test_savefilepath = '../../Datasets/Thermal_Fin/' + 'parameter_test_bnd_%d' %(self.num_testing_data) 
-            self.state_obs_test_savefilepath = '../../Datasets/Thermal_Fin/' + 'state_test_bnd_%d' %(self.num_testing_data)             
+            self.parameter_train_savefilepath = '../../Datasets/Thermal_Fin/' + 'parameter_train_bnd_%d' %(hyper_p.num_training_data) + parameter_type
+            self.state_obs_train_savefilepath = '../../Datasets/Thermal_Fin/' + 'state_train_bnd_%d' %(hyper_p.num_training_data) + parameter_type
+            self.parameter_test_savefilepath = '../../Datasets/Thermal_Fin/' + 'parameter_test_bnd_%d' %(self.num_testing_data) + parameter_type
+            self.state_obs_test_savefilepath = '../../Datasets/Thermal_Fin/' + 'state_test_bnd_%d' %(self.num_testing_data) + parameter_type        
         
         #=== Saving neural network ===#
         self.NN_savefile_directory = '../Trained_NNs/' + self.filename # Since we need to save four different types of files to save a neural network model, we need to create a new folder for each model
