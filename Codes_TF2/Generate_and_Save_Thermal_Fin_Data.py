fb3208c66710c9453b20c1a9cd4b3ae0e4104e0c
Hwan Goh <Hwan.Goh@gmail.com>
2019-11-15 22:29:16 -0600

    small update

--- Codes_TF2/Generate_and_Save_Thermal_Fin_Data.py
+++ Codes_TF2/Generate_and_Save_Thermal_Fin_Data.py
@@ -110,8 +110,8 @@ if __name__ == "__main__":
     generate_test_data = 1
 
     #===  Select observation type ===#
-    generate_full_domain = 1
-    generate_boundary_state = 0
+    generate_full_domain = 0
+    generate_boundary_state = 1
     
     #===  Defining filenames and creating directories ===#         
     if generate_nine_parameters == 1:
