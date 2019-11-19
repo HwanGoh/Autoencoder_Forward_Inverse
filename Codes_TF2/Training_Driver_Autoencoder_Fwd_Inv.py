fb09579c593ac53d68a0588f17b757d3eb71b63c
HwanGoh <Hwan.Goh@gmail.com>
2019-11-19 10:19:39 -0600

    small update

--- Codes_TF2/Training_Driver_Autoencoder_Fwd_Inv.py
+++ Codes_TF2/Training_Driver_Autoencoder_Fwd_Inv.py
@@ -25,9 +25,9 @@ np.random.seed(1234)
 ###############################################################################
 class HyperParameters:
     data_type         = 'full'
-    num_hidden_layers = 3
-    truncation_layer  = 2 # Indexing includes input and output layer with input layer indexed by 0
-    num_hidden_nodes  = 50
+    num_hidden_layers = 5
+    truncation_layer  = 3 # Indexing includes input and output layer with input layer indexed by 0
+    num_hidden_nodes  = 500
     penalty           = 50
     batch_size        = 1000
     num_epochs        = 500
@@ -36,16 +36,16 @@ class HyperParameters:
 class RunOptions:
     def __init__(self, hyper_p): 
         #=== Data Set ===#
-        data_thermal_fin_nine = 0
-        data_thermal_fin_vary = 1
+        data_thermal_fin_nine = 1
+        data_thermal_fin_vary = 0
         
         #=== Data Set Size ===#
         self.num_training_data = 50000
         self.num_testing_data = 200
         
         #=== Data Dimensions ===#
-        self.fin_dimensions_2D = 1
-        self.fin_dimensions_3D = 0
+        self.fin_dimensions_2D = 0
+        self.fin_dimensions_3D = 1
         
         #=== Random Seed ===#
         self.random_seed = 1234
