5ea8e54d543c97c86db29df5bc7ca6fc22afd431
HwanGoh <Hwan.Goh@gmail.com>
2019-11-19 14:22:05 -0600

    small update

--- Codes_TF2/Training_Driver_Autoencoder_Fwd_Inv.py
+++ Codes_TF2/Training_Driver_Autoencoder_Fwd_Inv.py
@@ -44,8 +44,8 @@ class RunOptions:
         self.num_testing_data = 200
         
         #=== Data Dimensions ===#
-        self.fin_dimensions_2D = 1
-        self.fin_dimensions_3D = 0
+        self.fin_dimensions_2D = 0
+        self.fin_dimensions_3D = 1
         
         #=== Random Seed ===#
         self.random_seed = 1234
