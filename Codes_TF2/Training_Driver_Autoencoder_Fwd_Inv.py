2f2af673fe612bc05518f641961943752f4274bb
HwanGoh <Hwan.Goh@gmail.com>
2019-11-20 10:49:06 -0600

    small update

--- Codes_TF2/Training_Driver_Autoencoder_Fwd_Inv.py
+++ Codes_TF2/Training_Driver_Autoencoder_Fwd_Inv.py
@@ -27,11 +27,11 @@ class HyperParameters:
     data_type         = 'bnd'
     num_hidden_layers = 7
     truncation_layer  = 4 # Indexing includes input and output layer with input layer indexed by 0
-    num_hidden_nodes  = 500
+    num_hidden_nodes  = 1000
     penalty           = 50
     batch_size        = 1000
     num_epochs        = 2000
-    gpu               = '3'
+    gpu               = '2'
     
 class RunOptions:
     def __init__(self, hyper_p): 
