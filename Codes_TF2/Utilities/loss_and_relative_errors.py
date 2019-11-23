bd0c4eb8b70e9a3b5250f22fe96923d0c546d8cf
Hwan Goh <Hwan.Goh@gmail.com>
2019-11-22 21:12:02 -0600

    separated out run_options and file_paths!

---
+++ Codes_TF2/Utilities/loss_and_relative_errors.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 22 20:42:39 2019
+
+@author: hwan
+"""
+
+import tensorflow as tf
+
+###############################################################################
+#                                   Loss                                      #
+###############################################################################
+def loss_autoencoder(autoencoder_pred, parameter_true):
+    return tf.reduce_mean(tf.norm(tf.subtract(parameter_true, autoencoder_pred),2))
+
+def loss_forward_problem(state_obs_pred, state_obs_true, penalty):
+    return penalty*tf.reduce_mean(tf.norm(tf.subtract(state_obs_pred, state_obs_true),2))
+
+###############################################################################
+#                               Relative Error                                #
+###############################################################################
+def relative_error(prediction, true):
+    return tf.norm(true - prediction, 2)/tf.norm(true, 2)
