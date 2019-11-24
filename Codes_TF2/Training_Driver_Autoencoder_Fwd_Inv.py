b9d4eabe7fed3769dd7035ed5468a356741e5f45
Hwan Goh <Hwan.Goh@gmail.com>
2019-11-23 22:39:06 -0600

    small update

--- Codes_TF2/Training_Driver_Autoencoder_Fwd_Inv.py
+++ Codes_TF2/Training_Driver_Autoencoder_Fwd_Inv.py
@@ -126,7 +126,7 @@ def trainer(hyperp, run_options, file_paths):
     if run_options.distributed_training == 0:
         #=== Which GPU to Use ===#
         os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID" 
-        os.environ["CUDA_VISIBLE_DEVICES"] = run_options.dist_which_gpus
+        os.environ["CUDA_VISIBLE_DEVICES"] = run_options.which_gpu
         
         #=== Neural Network ===#
         NN = AutoencoderFwdInv(hyperp, parameter_dimension, run_options.full_domain_dimensions, obs_indices)
@@ -144,7 +144,10 @@ def trainer(hyperp, run_options, file_paths):
     if run_options.distributed_training == 1:
         #=== Which GPU to Use ===#
         os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID" 
-        os.environ["CUDA_VISIBLE_DEVICES"] = run_options.which_gpu
+        os.environ["CUDA_VISIBLE_DEVICES"] = run_options.dist_which_gpus
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        print(gpus)
+        pdb.set_trace
         dist_strategy = tf.distribute.MirroredStrategy()
         with dist_strategy.scope():
             #=== Neural Network ===#
