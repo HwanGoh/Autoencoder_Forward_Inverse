647640941c63a6b21621f323b8eb009976119040
Hwan Goh <Hwan.Goh@gmail.com>
2019-12-01 21:05:25 -0600

    small update

--- Codes_TF2/Training_Driver_Autoencoder_Fwd_Inv.py
+++ Codes_TF2/Training_Driver_Autoencoder_Fwd_Inv.py
@@ -24,14 +24,14 @@ import pdb #Equivalent of keyboard in MATLAB, just add "pdb.set_trace()"
 #                       Hyperparameters and Run_Options                       #
 ###############################################################################
 class Hyperparameters:
-    data_type         = 'full'
+    data_type         = 'bnd'
     num_hidden_layers = 5
     truncation_layer  = 3 # Indexing includes input and output layer with input layer indexed by 0
-    num_hidden_nodes  = 519
+    num_hidden_nodes  = 500
     activation        = 'relu'
     penalty           = 1
     batch_size        = 1000
-    num_epochs        = 1000
+    num_epochs        = 10000
     
 class RunOptions:
     def __init__(self): 
@@ -45,8 +45,8 @@ class RunOptions:
         self.which_gpu = '3'
         
         #=== Data Set ===#
-        self.data_thermal_fin_nine = 1
-        self.data_thermal_fin_vary = 0
+        self.data_thermal_fin_nine = 0
+        self.data_thermal_fin_vary = 1
         
         #=== Data Set Size ===#
         self.num_data_train = 50000
