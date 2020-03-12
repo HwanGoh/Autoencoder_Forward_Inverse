5bd358a7c35abae8a585b29f5f4288bf337c3398
Hwan Goh <Hwan.Goh@gmail.com>
2020-03-12 16:56:42 -0500

    small update

--- Codes_TF2/Prediction_Driver_Thermal_Fin.py
+++ Codes_TF2/Prediction_Driver_Thermal_Fin.py
@@ -43,8 +43,8 @@ class RunOptions:
         self.use_model_induced = 0
         
         #=== Data Set ===#
-        self.data_thermal_fin_nine = 0
-        self.data_thermal_fin_vary = 1
+        self.data_thermal_fin_nine = 1
+        self.data_thermal_fin_vary = 0
         
         #=== Data Set Size ===#
         self.num_data_train = 50000
