a737a5094368fe8bebec1e141fc3065fc1545fed
Hwan Goh <Hwan.Goh@gmail.com>
2020-04-06 11:13:22 -0500

    updated prediction and plotting drivers for model induced probelm

--- Codes_TF2/Prediction_Driver_Thermal_Fin.py
+++ Codes_TF2/Prediction_Driver_Thermal_Fin.py
@@ -19,15 +19,15 @@ import pdb #Equivalent of keyboard in MATLAB, just add "pdb.set_trace()"
 #                       Hyperparameters and Run_Options                       #
 ###############################################################################
 class Hyperparameters:
-    data_type         = 'full'
+    data_type         = 'bnd'
     num_hidden_layers = 5
     truncation_layer  = 3 # Indexing includes input and output layer with input layer indexed by 0
     num_hidden_nodes  = 500
     activation        = 'relu'
-    penalty_encoder   = 1.00
-    penalty_decoder   = 1.00
-    penalty_aug       = 0
-    penalty_prior     = 0.0
+    penalty_encoder   = 1
+    penalty_decoder   = 0
+    penalty_aug       = 50
+    penalty_prior     = 0
     batch_size        = 1000
     num_epochs        = 1000
     
@@ -38,9 +38,9 @@ class RunOptions:
         self.use_reverse_autoencoder = 0
         
         #=== Autoencoder Loss ===#
-        self.use_model_aware = 1
+        self.use_model_aware = 0
         self.use_model_augmented = 0
-        self.use_model_induced = 0
+        self.use_model_induced = 1
         
         #=== Data Set ===#
         self.data_thermal_fin_nine = 0
@@ -122,10 +122,10 @@ class FilePaths():
             penalty_decoder_string = 'pt' + penalty_decoder_string[2:]
         if hyperp.penalty_aug >= 1:
             hyperp.penalty_aug = int(hyperp.penalty_aug)
-            penalty_string_aug = str(hyperp.penalty_aug)
+            penalty_aug_string = str(hyperp.penalty_aug)
         else:
-            penalty_string_aug = str(hyperp.penalty_aug)
-            penalty_string_aug = 'pt' + penalty_string_aug[2:]  
+            penalty_aug_string = str(hyperp.penalty_aug)
+            penalty_aug_string = 'pt' + penalty_aug_string[2:]  
         if hyperp.penalty_prior >= 1:
             hyperp.penalty_proor = int(hyperp.penalty_prior)
             penalty_prior_string = str(hyperp.penalty_prior)
@@ -137,9 +137,9 @@ class FilePaths():
         if run_options.use_model_aware == 1:
             self.filename = self.autoencoder_type + self.autoencoder_loss + '_' + self.dataset + self.N_Nodes + '_' + hyperp.data_type + fin_dimension + '_hl%d_tl%d_hn%d_%s_en%s_de%s_pr%s_d%d_b%d_e%d' %(hyperp.num_hidden_layers, hyperp.truncation_layer, hyperp.num_hidden_nodes, hyperp.activation, penalty_encoder_string, penalty_decoder_string, penalty_prior_string, run_options.num_data_train, hyperp.batch_size, hyperp.num_epochs)
         if run_options.use_model_augmented == 1:
-            self.filename = self.autoencoder_type + self.autoencoder_loss + '_' + self.dataset + self.N_Nodes + '_' + hyperp.data_type + fin_dimension + '_hl%d_tl%d_hn%d_%s_p%s_d%d_b%d_e%d' %(hyperp.num_hidden_layers, hyperp.truncation_layer, hyperp.num_hidden_nodes, hyperp.activation, penalty_string_aug, run_options.num_data_train, hyperp.batch_size, hyperp.num_epochs)
+            self.filename = self.autoencoder_type + self.autoencoder_loss + '_' + self.dataset + self.N_Nodes + '_' + hyperp.data_type + fin_dimension + '_hl%d_tl%d_hn%d_%s_p%s_d%d_b%d_e%d' %(hyperp.num_hidden_layers, hyperp.truncation_layer, hyperp.num_hidden_nodes, hyperp.activation, penalty_aug_string, run_options.num_data_train, hyperp.batch_size, hyperp.num_epochs)
         if run_options.use_model_induced == 1:
-            self.filename = self.autoencoder_type + self.autoencoder_loss + '_' + self.dataset + self.N_Nodes + '_' + hyperp.data_type + fin_dimension + '_hl%d_tl%d_hn%d_%s_p%s_paug%s_pr%s_d%d_b%d_e%d' %(hyperp.num_hidden_layers, hyperp.truncation_layer, hyperp.num_hidden_nodes, hyperp.activation, penalty_encoder_string, penalty_string_aug, penalty_prior_string, run_options.num_data_train, hyperp.batch_size, hyperp.num_epochs)
+            self.filename = self.autoencoder_type + self.autoencoder_loss + '_' + self.dataset + self.N_Nodes + '_' + hyperp.data_type + fin_dimension + '_hl%d_tl%d_hn%d_%s_en%s_de%s_aug%s_pr%s_d%d_b%d_e%d' %(hyperp.num_hidden_layers, hyperp.truncation_layer, hyperp.num_hidden_nodes, hyperp.activation, penalty_encoder_string, penalty_decoder_string, penalty_aug_string, penalty_prior_string, run_options.num_data_train, hyperp.batch_size, hyperp.num_epochs)
         
         #=== Loading and Saving Data ===#
         self.observation_indices_savefilepath = '../../Datasets/Thermal_Fin/' + 'obs_indices' + '_' + hyperp.data_type + self.N_Nodes + fin_dimension
