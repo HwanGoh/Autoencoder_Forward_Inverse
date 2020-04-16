updated all training codes to include gradient norms and fixed typos

Codes_TF2/Utilities/optimize_model_aware_autoencoder.py
@@ -211,7 +211,7 @@ def optimize(hyperp, run_options, file_paths, NN, loss_autoencoder, loss_encoder
         storage_array_relative_error_data_autoencoder = np.append(storage_array_relative_error_data_autoencoder, mean_relative_error_data_autoencoder.result())
         storage_array_relative_error_latent_encoder = np.append(storage_array_relative_error_latent_encoder, mean_relative_error_latent_encoder.result())
         storage_array_relative_error_data_decoder = np.append(storage_array_relative_error_data_decoder, mean_relative_error_data_decoder.result())
-        storage_array_relative_gradients = np.append(storage_array_relative_gradients, relative_gradient_norm) 
+        storage_array_relative_gradient_norm = np.append(storage_array_relative_gradient_norm, relative_gradient_norm) 
 
         #=== Display Epoch Iteration Information ===#
         elapsed_time_epoch = time.time() - start_time_epoch
@@ -220,7 +220,7 @@ def optimize(hyperp, run_options, file_paths, NN, loss_autoencoder, loss_encoder
         print('Val Loss: Full: %.3e, AE: %.3e, Encoder: %.3e, Decoder: %.3e' %(mean_loss_val.result(), mean_loss_val_autoencoder.result(), mean_loss_val_encoder.result(), mean_loss_val_decoder.result()))
         print('Test Loss: Full: %.3e, AE: %.3e, Encoder: %.3e, Decoder: %.3e' %(mean_loss_test.result(), mean_loss_test_autoencoder.result(), mean_loss_test_encoder.result(), mean_loss_test_decoder.result()))
         print('Rel Errors: AE: %.3e, Encoder: %.3e, Decoder: %.3e' %(mean_relative_error_data_autoencoder.result(), mean_relative_error_latent_encoder.result(), mean_relative_error_data_decoder.result()))
-        print('Relative Gradient Norm: %.4f\n' %(relative_gradient_norm)
+        print('Relative Gradient Norm: %.4f\n' %(relative_gradient_norm))
         start_time_epoch = time.time()
         
         #=== Resetting Metrics ===#
@@ -246,15 +246,16 @@ def optimize(hyperp, run_options, file_paths, NN, loss_autoencoder, loss_encoder
             metrics_dict = {}
             metrics_dict['loss_train'] = storage_array_loss_train
             metrics_dict['loss_train_autoencoder'] = storage_array_loss_train_autoencoder
-            metrics_dict['loss_train_forward_problem'] = storage_array_loss_train_encoder
-            metrics_dict['loss_train_inverse_problem'] = storage_array_loss_train_decoder
+            metrics_dict['loss_train_encoder'] = storage_array_loss_train_encoder
+            metrics_dict['loss_train_decoder'] = storage_array_loss_train_decoder
             metrics_dict['loss_val'] = storage_array_loss_val
             metrics_dict['loss_val_autoencoder'] = storage_array_loss_val_autoencoder
-            metrics_dict['loss_val_forward_problem'] = storage_array_loss_val_encoder
-            metrics_dict['loss_val_inverse_problem'] = storage_array_loss_val_decoder
-            metrics_dict['relative_error_parameter_autoencoder'] = storage_array_relative_error_data_autoencoder
-            metrics_dict['relative_error_state_obs'] = storage_array_relative_error_latent_encoder
-            metrics_dict['relative_error_parameter_inverse_problem'] = storage_array_relative_error_data_decoder
+            metrics_dict['loss_val_encoder'] = storage_array_loss_val_encoder
+            metrics_dict['loss_val_decoder'] = storage_array_loss_val_decoder
+            metrics_dict['relative_error_data_autoencoder'] = storage_array_relative_error_data_autoencoder
+            metrics_dict['relative_error_latent_encoder'] = storage_array_relative_error_latent_encoder
+            metrics_dict['relative_error_data_decoder'] = storage_array_relative_error_data_decoder
+            metrics_dict['relative_gradient_norm'] = storage_array_relative_gradient_norm
             df_metrics = pd.DataFrame(metrics_dict)
             df_metrics.to_csv(file_paths.NN_savefile_name + "_metrics" + '.csv', index=False)
             print('Current Model and Metrics Saved') 