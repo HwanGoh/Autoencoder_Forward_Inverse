fixed up tensorboard outputs for relative gradients

Codes_TF2/Utilities/optimize_model_induced_autoencoder.py
@@ -256,8 +256,8 @@ def optimize(hyperp, run_options, file_paths, NN, obs_indices, loss_autoencoder,
                 sum_gradient_norms += l2_norm(gradient)
                 if epoch == 0:
                     initial_sum_gradient_norms = sum_gradient_norms
-            tf.summary.scalar('sum_gradient_norms', sum_gradient_norms, step=epoch)         
             relative_gradient_norm = sum_gradient_norms/initial_sum_gradient_norms
+            tf.summary.scalar('relative_gradient_norm', relative_gradient_norm, step=epoch)         
 
         #=== Update Storage Arrays ===#
         storage_array_loss_train = np.append(storage_array_loss_train, mean_loss_train.result())