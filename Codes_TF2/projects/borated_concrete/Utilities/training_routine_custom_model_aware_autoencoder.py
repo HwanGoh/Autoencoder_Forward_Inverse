fb933c73c7de528f20f11be173723ca2450ef7f2
Hwan Goh <Hwan.Goh@gmail.com>
2020-07-05 17:05:12 -0500

    updated distributed training

--- Codes_TF2/projects/borated_concrete/Utilities/training_routine_custom_model_aware_autoencoder.py
+++ Codes_TF2/projects/borated_concrete/Utilities/training_routine_custom_model_aware_autoencoder.py
@@ -110,6 +110,6 @@ def trainer_custom(hyperp, run_options, file_paths):
                 hyperp, run_options, file_paths,
                 NN, optimizer,
                 loss_penalized_difference, relative_error,
+                reg_prior, prior_mean, prior_covariance_cholesky,
                 input_and_latent_train, input_and_latent_val, input_and_latent_test,
                 input_dimensions, num_batches_train)
-
