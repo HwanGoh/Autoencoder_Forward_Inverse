c05b7fc3dd1710a0f0ed92856b35d1a6528312e7
Hwan Goh <Hwan.Goh@gmail.com>
2020-08-19 23:40:27 -0500

    added scheduler codes for Poisson_2D

---
+++ Codes_TF2/projects/Poisson_2D/Scheduler_Prediction_and_Plotting_AE_Model_Aware.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 18 20:53:06 2019
+
+@author: Jon Wittmer
+"""
+
+import os
+import sys
+sys.path.insert(0, os.path.realpath('../../src'))
+import subprocess
+import copy
+from get_hyperparameter_permutations import get_hyperparameter_permutations
+from Prediction_and_Plotting_Driver import Hyperparameters
+
+import pdb #Equivalent of keyboard in MATLAB, just add "pdb.set_trace()"
+
+###############################################################################
+#                                   Executor                                  #
+###############################################################################
+if __name__ == '__main__':
+
+    #########################
+    #   Get Scenarios List  #
+    #########################
+    hyperp = Hyperparameters() # Assign instance attributes below
+
+    # assign instance attributes for hyperp
+    hyperp.num_hidden_layers = [5]
+    hyperp.truncation_layer   = [3]
+    hyperp.num_hidden_nodes  = [500]
+    hyperp.activation        = ['relu']
+    hyperp.penalty_encoder   = [0.0001, 0.001, 0.01, 0.1]
+    hyperp.penalty_decoder   = [0.0001, 0.001, 0.01, 0.1]
+    hyperp.batch_size        = [100]
+    hyperp.num_epochs        = [1000]
+
+    permutations_list, hyperp_keys = get_hyperparameter_permutations(hyperp)
+    print('permutations_list generated')
+
+    # Convert each list in permutations_list into class attributes
+    scenarios_class_instances = []
+    for scenario_values in permutations_list:
+        hyperp_scenario = Hyperparameters()
+        for i in range(0, len(scenario_values)):
+            setattr(hyperp_scenario, hyperp_keys[i], scenario_values[i])
+        scenarios_class_instances.append(copy.deepcopy(hyperp_scenario))
+
+    for scenario in scenarios_class_instances:
+        proc = subprocess.Popen(['./Prediction_and_Plotting_Driver.py',
+            f'{data.num_hidden_layers}',
+            f'{data.truncation_layer}',
+            f'{data.num_hidden_nodes}',
+            f'{data.activation}',
+            f'{data.penalty_encoder:.9f}',
+            f'{data.penalty_decoder:.9f}',
+            f'{data.batch_size}',
+            f'{data.num_epochs}',
+        proc.wait()
+
+    print('All scenarios computed')
