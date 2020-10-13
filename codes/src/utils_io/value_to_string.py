8ee463eeaef9b51d451cca9619bc558749e7cb98
Hwan Goh <Hwan.Goh@gmail.com>
2020-10-12 21:26:18 -0500

    updated value_to_string function again

--- codes/src/utils_io/value_to_string.py
+++ codes/src/utils_io/value_to_string.py
@@ -11,22 +11,17 @@ def value_to_string(value):
     if value >= 1:
         value = int(value)
         string = str(value)
+    elif value == 0:
+        string = '0'
     else:
         value_decimal_form = '%.9f'%(value)
         string = 'pt'
 
-        found_nonzero_value_flag = 0
-        for n in range(2,9):
-            if found_nonzero_value_flag == 1:
-                if value_decimal_form[n] == '0': # If found a zero
-                    break
-            if value_decimal_form[n] == '0':
-                string += '0'
-            else:
-                string += value_decimal_form[n]
-                found_nonzero_value_flag = 1
+        for n in range(1,len(value_decimal_form)):
+            if value_decimal_form[-n] != '0':
+                index = n
+                break
 
-        if found_nonzero_value_flag != 1: # If no nonzeros found
-            string = '0'
+        string += value_decimal_form[2:12-index]
 
     return string
