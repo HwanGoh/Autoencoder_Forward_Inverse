bf0aa7aee2608a487bc6455e32d41fe88398dbeb
Hwan Goh <Hwan.Goh@gmail.com>
2020-11-23 16:40:16 -0600

    updated scheduler cases

--- codes/projects/poisson_2d/drivers_vae/scheduler_multinode_multiprocess_training_vae.py
+++ codes/projects/poisson_2d/drivers_vae/scheduler_multinode_multiprocess_training_vae.py
@@ -36,7 +36,7 @@ def generate_scenarios_list():
     hyperp['num_hidden_nodes_encoder']  = [500]
     hyperp['num_hidden_nodes_decoder']  = [500]
     hyperp['activation']                = ['tanh']
-    hyperp['penalty_js']                = [0.00001, 0.001, 0.1, 0.5, 0.99]
+    hyperp['penalty_js']                = [0.00001, 0.001, 0.1, 0.25, 0.5]
     hyperp['num_data_train']            = [500, 1000, 2500, 5000]
     hyperp['batch_size']                = [100]
     hyperp['num_epochs']                = [1000]
