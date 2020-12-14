dc6cbdf6d180137136a6fa9e7541b36b5d3d9b35
Hwan Goh <Hwan.Goh@gmail.com>
2020-12-14 13:00:47 -0600

    reverted back to mass matrix multiplication

--- codes/projects/elliptic_linear_1d/utils_project/get_fem_matrices_tf.py
+++ codes/projects/elliptic_linear_1d/utils_project/get_fem_matrices_tf.py
@@ -8,8 +8,12 @@ def load_fem_matrices_tf(options, filepaths):
     #=== Load Spatial Operator ===#
     df_forward_matrix = pd.read_csv(filepaths.project.forward_matrix + '.csv')
     forward_matrix = df_forward_matrix.to_numpy()
+    df_mass_matrix = pd.read_csv(filepaths.project.mass_matrix + '.csv')
+    mass_matrix = df_mass_matrix.to_numpy()
 
     forward_matrix =\
             forward_matrix.reshape((options.parameter_dimensions, options.parameter_dimensions))
+    mass_matrix =\
+            mass_matrix.reshape((options.parameter_dimensions, options.parameter_dimensions))
 
-    return tf.cast(forward_matrix, tf.float32)
+    return tf.cast(forward_matrix, tf.float32), tf.cast(mass_matrix, tf.float32)
